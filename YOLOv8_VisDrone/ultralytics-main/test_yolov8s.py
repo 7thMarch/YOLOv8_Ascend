c480db06387a4c74a697ada4acccf8336ed7c3f4
ZPJ-LEFT <2724682324@qq.com>
2025-04-17 00:09:31 +0800

    V1

--- YOLOv8_VisDrone/ultralytics-main/test_yolov8s.py
+++ YOLOv8_VisDrone/ultralytics-main/test_yolov8s.py
@@ -2,10 +2,10 @@ from PIL import Image
 
 from ultralytics import YOLO
 
-ckp_path = 'G:\\MyReposity\\YOLOv8_Ascend\\YOLO算法改进\\core\\checkpoints\\yolov8s.pt'
+ckp_path = 'E:\\MyReposity\\YOLOv8_Ascend\\YOLO算法改进\\core\\checkpoints\\yolov8s.pt'
 model = YOLO(ckp_path)
 
-img_path = 'G:\\MyReposity\\YOLOv8_Ascend\\YOLO算法改进\\data\\coco8\\images\\val\\000000000036.jpg'
+img_path = 'E:\\MyReposity\\YOLOv8_Ascend\\YOLO算法改进\\data\\coco8\\images\\val\\000000000036.jpg'
 im1 = Image.open(img_path)
 
 results = model.predict(source=im1)  # save plotted images
